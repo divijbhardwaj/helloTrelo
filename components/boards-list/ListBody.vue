f888ce6f217f43e5503712af208776771a6c20cf
divij <divij.827@gmail.com>
2020-12-30 22:30:06 +0530

    card ui

--- components/boards-list/ListBody.vue
+++ components/boards-list/ListBody.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="list--body">
     <!-- <div class="overflow-test"></div> -->
-    <draggable class="list-group" :list="listData.cards" group="people">
+    <draggable v-bind="dragOptions" class="cards-container" :list="listData.cards" group="people">
       <template v-for="(card, i) in listData.cards">
         <div class="card" :key="i">
-          {{card.text}}
+          <div class="card-content">
+            {{card.text}}
+          </div>
         </div>
       </template>
     </draggable>
@@ -25,6 +27,14 @@ export default {
       default: () => ({}),
     },
   },
+  computed: {
+    dragOptions: () => ({
+      animation: 60,
+      group: 'description',
+      disabled: false,
+      ghostClass: 'ghost',
+    }),
+  },
 };
 </script>
 
@@ -35,6 +45,29 @@ export default {
 
   .overflow-test{
     height: 1000px;
+  }
+  .cards-container {
+    width:100%;
+    display: flex;
+    flex-direction: column;
+    padding: 0 10px 10px;
+    .card{
+      cursor: pointer;
+      width:100%;
+      height: 32px;
+      background-color: white;
+      border-radius: 3px;
+      margin-bottom: 8px;
+      box-shadow: 0 1px 0 rgba(9,30,66,.25);
+      padding: 6px 8px 2px;
+
+      &:hover {
+        background-color: #f4f5f6;
+      }
+      &:last-child {
+        margin:0;
+      }
+    }
   }
 }
 </style>
